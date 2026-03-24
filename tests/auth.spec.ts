5957c98d5d6c5e872617b6d63df695332b5fb50c
mnkolloQAEngineer <mnkollo@gmail.com>
2026-03-23 21:55:45 -0500

    Update auth.spec.ts

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -3,33 +3,34 @@ import { LoginPage } from "../page/loginPage";
 import { HomePage } from "../page/homePage";
 
 test.describe("Testing Authentication", () => {
-  test.beforeEach(async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.goto();
-});
+  let loginPage: LoginPage;
+  let homePage: HomePage;
 
-  test("valid login", async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    const homePage = new HomePage(page);
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    homePage = new HomePage(page);
+    await loginPage.goto();
+  });
+
+  test("valid login", async () => {
     await loginPage.performLogin(process.env.USERNAME!, process.env.PASSWORD!);
     await expect(homePage.menuLabelDropdown).toBeVisible();
-  })
-  test("invalid login shows error", async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.performLogin("invalidUser", "invalidPass");
-    await expect(loginPage.loginFailureMessage).toHaveText('Invalid username or password');
-  })
-  test('blank credentials show login failure', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.performLogin("", "");
-    await expect(loginPage.loginFailureMessage).toHaveText('Invalid username or password');
   });
-  test('logout after login', async ({ page }) => {
-    const loginPage = new LoginPage(page);
+
+  test("invalid login shows error", async () => {
+    await loginPage.performLogin("invalidUser", "invalidPass");
+    await expect(loginPage.loginFailureMessage).toHaveText("Invalid username or password");
+  });
+
+  test("blank credentials show login failure", async () => {
+    await loginPage.performLogin("", "");
+    await expect(loginPage.loginFailureMessage).toHaveText("Invalid username or password");
+  });
+
+  test("logout after login", async () => {
     await loginPage.performLogin(process.env.USERNAME!, process.env.PASSWORD!);
-    await expect(page).toHaveURL(/main/);
-    const homePage = new HomePage(page);
+    await expect(homePage.menuLabelDropdown).toBeVisible();
     await homePage.performLogout();
     await expect(loginPage.loginForm).toBeVisible();
-  })
-})
+  });
+});
